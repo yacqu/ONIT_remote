e5be0685422ae29b7eff2e9ee634188993bfefcc
yacqub abdirahman <yacqubyacqub@icloud.com>
2022-10-24 21:44:25 -0700

    This is some text

--- ONIT/ContentView.swift
+++ ONIT/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Y'all")
+        ScrollView {
+                Text("Im him")
+                .font(.tittle)
+                .bold()
+                .padding(.top)
         }
-        .padding()
+        .navigationTittle("Discover")
     }
 }
 
