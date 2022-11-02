20341fa979eda262a811911cfb6d2cc017cae8a5
MACBOOK <yacqubyacqub@icloud.com>
2022-11-02 12:45:04 -0700

    Addition of images and restriction icons

--- ONIT/ItemRow.swift
+++ ONIT/ItemRow.swift
@@ -9,9 +9,29 @@ import SwiftUI
 
 struct ItemRow: View {
     let item: MenuItem
-    
-    var body: some View {
-        Text(item.name)
+    let colors: [String: Color] = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green]
+
+    var body: some View{
+        HStack {
+            Image(item.thumbnailImage).clipShape(Circle())
+                .overlay(Circle().stroke(.gray, lineWidth: 2))
+            VStack(alignment:.leading) {
+                Text(item.name).font(.headline)
+                Text("$\(item.price)")
+            }
+            Spacer()
+            ForEach (item.restrictions, id: \.self) {restrictions in
+                Text(restrictions)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(colors[restrictions, default: .black])
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
+                   
+                    
+            }
+        }
     }
 }
 
